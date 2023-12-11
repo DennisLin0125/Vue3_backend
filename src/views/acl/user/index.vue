d34210b4197a6145f04f6743399b633e4ac5d76e
Dennis <740125aa@gmail.com>
2023-12-11 20:50:06 +0800

    完成添加新的帳號業務

--- src/views/acl/user/index.vue
+++ src/views/acl/user/index.vue
@@ -11,7 +11,7 @@
     </el-form>
   </el-card>
   <el-card style="margin: 10px 0">
-    <el-button type="primary">添加</el-button>
+    <el-button type="primary" @click="addUser">添加用戶</el-button>
     <el-button type="danger">批量刪除</el-button>
     <el-table border style="margin: 10px 0" :data="userArr">
       <el-table-column type="selection" width="80" align="center" />
@@ -55,22 +55,54 @@
           <el-button type="primary" icon="User" size="small">
             分配角色
           </el-button>
-          <el-button type="info" icon="Edit" size="small">編輯</el-button>
+          <el-button
+            type="info"
+            icon="Edit"
+            size="small"
+            @click="updateUser(row)"
+          >
+            編輯
+          </el-button>
           <el-button type="danger" icon="Delete" size="small">刪除</el-button>
         </template>
       </el-table-column>
     </el-table>
+    <el-pagination
+      v-model:current-page="page"
+      v-model:page-size="size"
+      :page-sizes="[3, 5, 10]"
+      background
+      layout="prev, pager, next, jumper,->, sizes,total"
+      :total="total"
+      @size-change="handleSizeChange"
+      @current-change="getHasUser"
+    />
   </el-card>
-  <el-pagination
-    v-model:current-page="page"
-    v-model:page-size="size"
-    :page-sizes="[3, 5, 10]"
-    background
-    layout="prev, pager, next, jumper,->, sizes,total"
-    :total="total"
-    @size-change="handleSizeChange"
-    @current-change="getHasUser"
-  />
+  <!--  抽屜-->
+  <el-drawer v-model="drawer">
+    <template #header>
+      <h4>添加用戶</h4>
+    </template>
+    <template #default>
+      <el-form>
+        <el-form-item label="用戶姓名">
+          <el-input placeholder="請輸入名字" />
+        </el-form-item>
+        <el-form-item label="用戶暱稱">
+          <el-input placeholder="請輸入暱稱" />
+        </el-form-item>
+        <el-form-item label="用戶密碼">
+          <el-input placeholder="請輸入密碼" />
+        </el-form-item>
+      </el-form>
+    </template>
+    <template #footer>
+      <div style="flex: auto">
+        <el-button>取消</el-button>
+        <el-button type="primary">確定</el-button>
+      </div>
+    </template>
+  </el-drawer>
 </template>
 
 <script lang="ts">
@@ -82,13 +114,14 @@ export default {
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
 import { reqUserInfo } from '@/api/acl/user'
-import type { UserResponseData, Records } from '@/api/acl/user/type.ts'
+import type { UserResponseData, Records, User } from '@/api/acl/user/type.ts'
 
 let page = ref<number>(1)
 let size = ref<number>(5)
 let total = ref<number>(10)
 let keyword = ref<string>('')
 let userArr = ref<Records>([])
+let drawer = ref<boolean>(false)
 
 onMounted(() => {
   getHasUser()
@@ -109,6 +142,14 @@ const getHasUser = async (pager = 1) => {
 
 const handleSizeChange = () => {
   getHasUser()
+}
+
+const addUser = () => {
+  drawer.value = true
+}
+
+const updateUser = (row: User) => {
+  drawer.value = true
 }
 </script>
 

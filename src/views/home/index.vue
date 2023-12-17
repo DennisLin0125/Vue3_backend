af9e81f9fd5ba900397df40d28fd989c288b21ed
Dennis <740125aa@gmail.com>
2023-12-17 11:44:38 +0800

    首頁完成

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,15 @@
 <template>
-  <div>
-    <h1>home</h1>
+  <el-card>
+    <div class="box">
+      <img :src="userStore.avatar" alt="" class="avatar" />
+      <div class="bottom">
+        <h3 class="title">{{ getTime() }}好呀!!{{ userStore.username }}</h3>
+        <p class="subTitle">後台管理平台</p>
+      </div>
+    </div>
+  </el-card>
+  <div class="svgBottom">
+    <svg-icon name="welcome" width="600px" height="300px" />
   </div>
 </template>
 
@@ -9,6 +18,42 @@ export default {
   name: 'Home',
 }
 </script>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import useUserStore from '@/store/modules/user.ts'
+import { getTime } from '@/utils/time.ts'
 
-<style scoped lang="scss"></style>
+let userStore = useUserStore()
+</script>
+
+<style scoped lang="scss">
+.box {
+  display: flex;
+
+  .avatar {
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+  }
+
+  .bottom {
+    margin-left: 20px;
+
+    .title {
+      margin-bottom: 30px;
+      font-size: 30px;
+      font-weight: 900;
+    }
+
+    .subTitle {
+      font-style: italic;
+      color: silver;
+    }
+  }
+}
+
+.svgBottom {
+  display: flex;
+  justify-content: center;
+  margin-top: 100px;
+}
+</style>
